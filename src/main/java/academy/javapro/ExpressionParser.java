c66efd5e8fa5f53b7164f23d5760b92ef5845c55
Nathaniel Rankine <nathanielrankine1@gmail.com>
2025-02-24 12:52:54 -0500

    finished homework

--- src/main/java/academy/javapro/ExpressionParser.java
+++ src/main/java/academy/javapro/ExpressionParser.java
@@ -11,55 +11,86 @@ class ExpressionParser {
 
     // expr → expr + term
     public double parseExpression() {
-        // TODO: Get the leftmost term value by calling parseTerm()
-        // TODO: While we haven't reached the end of input and current char is '+'
-        // TODO: Increment position to skip the '+' operator
-        // TODO: Get the next term on the right
-        // TODO: Add the right term to our running total
-        // TODO: Return the final value
-        throw new UnsupportedOperationException("Implement parseExpression");
+        double result = parseTerm();
+        while (position < input.length() && input.charAt(position) == '+') {
+            position++;
+            double rightTerm = parseTerm();
+            result += rightTerm;
+        }
+        return result;
     }
 
     // term → term * factor
     private double parseTerm() {
-        // TODO: Get the leftmost factor value by calling parseFactor()
-        // TODO: While we haven't reached the end of input and current char is '*'
-        // TODO: Increment position to skip the '*' operator
-        // TODO: Get the next factor on the right
-        // TODO: Multiply the right factor with our running total
-        // TODO: Return the final value
-        throw new UnsupportedOperationException("Implement parseTerm");
+        double result = parseFactor();
+        while (position < input.length() && input.charAt(position) == '*') {
+            position++;
+            double rightFactor = parseFactor();
+            result *= rightFactor;
+        }
+        return result;
     }
 
     // factor → ( expr )
     private double parseFactor() {
-        // TODO: Check if we have an opening parenthesis and haven't reached end of input
-        // TODO: Increment position to skip the opening parenthesis
-        // TODO: Parse the expression inside the parentheses
-        // TODO: Increment position to skip the closing parenthesis
-        // TODO: Return the result of the parenthesized expression
-        // TODO: If no parentheses, parse and return a number
-        throw new UnsupportedOperationException("Implement parseFactor");
+        if (position < input.length() && input.charAt(position) == '(') {
+            position++;
+            double result = parseExpression();
+            if (position >= input.length() || input.charAt(position) != ')') {
+                throw new RuntimeException(String.format("Missing closing parenthesis at this position: %d", position));
+            }
+            position++;
+            return result;
+        } else if (position < input.length() && (Character.isDigit(input.charAt(position))
+                || input.charAt(position) == '.')) {
+            return parseNumber();
+        } else {
+            throw new RuntimeException(
+                    String.format("Unexpected character at position %d", position));
+        }
     }
 
     // Parse a numeric value
     private double parseNumber() {
-        // TODO: Create a StringBuilder to collect digits
-        // TODO: While we haven't reached the end and current char is digit or decimal point
-        // TODO: Append the current digit to our number string
-        // TODO: Move to next character
-        // TODO: Convert the string of digits to a double and return it
-        throw new UnsupportedOperationException("Implement parseNumber");
+        if (position >= input.length() ||
+                (!Character.isDigit(input.charAt(position)) && input.charAt(position) != '.')) {
+            throw new RuntimeException(String.format("Expected number at this position: %d", position));
+        }
+
+        StringBuilder number = new StringBuilder();
+        boolean hasDecimalPoint = false;
+
+        while (position < input.length() &&
+                (Character.isDigit(input.charAt(position)) || input.charAt(position) == '.')) {
+            if (input.charAt(position) == '.') {
+                if (hasDecimalPoint) {
+                    throw new RuntimeException(
+                            String.format("Multiple decimal points at the position %d", position));
+                }
+                hasDecimalPoint = true;
+            }
+            number.append(input.charAt(position));
+            position++;
+        }
+
+        try {
+            return Double.parseDouble(number.toString());
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(
+                    String.format("Invalid format at position %d: %s", position, number));
+        }
     }
 
     public static void main(String[] args) {
         // Test cases
         String[] testCases = {
-                "2 + 3 * (4 + 5)",    // Complex expression with parentheses
-                "2 + 3 * 4",          // Basic arithmetic with precedence
-                "(2 + 3) * 4",        // Parentheses changing precedence
+                "2 + 3 * (4 + 5)", // Complex expression with parentheses
+                "2 + 3 * 4", // Basic arithmetic with precedence
+                "(2 + 3) * 4", // Parentheses changing precedence
                 "2 * (3 + 4) * (5 + 6)", // Multiple parentheses
-                "1.5 + 2.5 * 3"       // Decimal numbers
+                "1.5 + 2.5 * 3", // Decimal numbers
+                "47 * ",
+                "1 + (2 + 3"
         };
 
         // Process each test case
